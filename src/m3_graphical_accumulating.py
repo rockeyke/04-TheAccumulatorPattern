90dccf9442fd99b360824d15da21cd27151f2a5c
rockeyke <rockeyke@rose-hulman.edu>
2018-12-03 16:04:35 -0500

    Done

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Kirsten Rockey.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -96,7 +96,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -109,6 +109,15 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+
+    mover = 0
+    for k in range(n+1):
+        last = rg.Point(point.x+length, point.y)
+        line = rg.Line(point, last)
+        line.attach_to(window)
+
+        point = rg.Point(point.x, point.y+30)
+    window.render()
 
 
 def run_test_draw_lines():
@@ -174,6 +183,16 @@ def draw_lines(n, point, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+
+    spacing = -100
+    for k in range(n):
+        last = rg.Point(point.x+100, point.y-(spacing))
+        line = rg.Line(point, last)
+        line.attach_to(window)
+
+        spacing = spacing+(200/(n-1))
+
+    window.render()
 
 
 # -----------------------------------------------------------------------------
